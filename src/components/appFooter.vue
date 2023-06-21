7f8d737565636a35688dc6a5514e4ce54ee8892c
Gianluca-Basili <gianluc.basili@gmail.com>
2023-06-21 19:31:19 +0200

    add footer

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -4,10 +4,40 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        footer
+    <div class="position-4 flex">
+        <div class="container left flex">
+            <button class="align-center">SIGN-UP DOWN</button>
+
+        </div>
+        <div class="container right flex align-center">
+            <h4 class="mr">FOLLOW US</h4>
+            <img src="../assets/img/footer-facebook.png" alt="" class="mr">
+            <img src="../assets/img/footer-periscope.png" alt="" class="mr">
+            <img src="../assets/img/footer-pinterest.png" alt="" class="mr">
+            <img src="../assets/img/footer-twitter.png" alt="" class="mr">
+            <img src="../assets/img/footer-youtube.png" alt="" class="mr">
+
+        </div>
     </div>
 </template>
-<style lang="">
+<style lang="scss">
+    .position-4{
+        position: absolute;
+        top: 770px;
+        width: 100%;
+        background-color:rgb(44, 44, 44);
+        padding: 20px;
+    }
+
+    button{
+        border: 3px solid blue;
+        color: white;
+        background-color: transparent;
+    }
+
+    h4{
+        color: blue;
+    }
     
+
 </style>
