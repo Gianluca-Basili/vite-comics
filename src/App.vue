d7b9ab0239cb5a678342e0a37ef1b024bedb67f5
Gianluca-Basili <gianluc.basili@gmail.com>
2023-06-21 15:22:17 +0200

    first push

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import appHeader from './components/appHeader.vue';
+import appContent from './components/appContent.vue';
+import appFooter from './components/appFooter.vue';
+
+export default {
+  components:{
+    appHeader,
+    appContent,
+    appFooter
+    
+  }
+}
+</script>
+<template lang="">
+  <div>
+    <appHeader/>
+    <appContent/>
+    <appFooter/>
+  </div>
+</template>
+    
+<style lang="">
+  
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div>
+        <a href="/">
+            <img src="../assets/img/dc-logo.png">
+        </a>
+    </div>
+</template>
+<style lang="">
+    
+</style>
