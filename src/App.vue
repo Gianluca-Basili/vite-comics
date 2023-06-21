f9be0ec202e2e55efcf5c13d7950444b5c624128
Gianluca-Basili <gianluc.basili@gmail.com>
2023-06-21 18:42:07 +0200

    add content

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import appHeader from './components/appHeader.vue';
 import appContent from './components/appContent.vue';
 import appFooter from './components/appFooter.vue';
 
+
 export default {
   components:{
     appHeader,
--- src/components/appContent.vue
+++ src/components/appContent.vue
@@ -4,10 +4,144 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        content
-    </div>
+    <main class="position-relative">
+        <div class=" position-absolute">
+            <div class="container padding">
+               CONTENT GOES HERE
+
+            </div>
+        </div>
+        <div class=" position-2 flex">
+            <div class="container padding flex align-center">
+               <img src="../assets/img/buy-comics-digital-comics.png" height="50">
+                <span class="ml">DIGITAL COMICS</span>
+                <div class=" padding flex align-center">
+                   <img src="../assets/img/buy-comics-merchandise.png" height="50">
+                    <span class="ml">DC MERCHANDISE</span>
+                </div>
+                <div class=" padding flex align-center">
+                    <img src="../assets/img/buy-comics-subscriptions.png" height="50">
+                    <span class="ml">SUBSCRIPTION</span>
+                </div>
+                <div class=" padding flex align-center">
+                   <img src="../assets/img/buy-comics-shop-locator.png" height="50">
+                    <span class="ml">COMIC SHOP LOCATOR</span>
+                </div>
+                <div class=" padding flex align-center">
+                   <img src="../assets/img/buy-dc-power-visa.svg" height="50">
+                    <span class="ml">DC POWER VISA</span>
+                </div>
+            </div>
+        </div>
+        <div class="position-3 background">
+            <div class="container-left flex">
+                <div class="container"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <h5>CHARACTERS</h5>
+                            <p>comics</p>
+                            <p>movies</p>
+                            <p>tv</p>
+                            <p>games</p>
+                            <p>videos</p>
+                            <p>news</p>
+                            <h5>SHOP</h5>
+                            <p>shop DC</p>
+                            <p>shop DC collectibles</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container flex">
+                    <div class="row">
+                        <div class="col">
+                            <h5>DC</h5>
+                            <p>terms of use</p>
+                            <p>privacy policy(New)</p>
+                            <p>Ad Choices</p>
+                            <p>Advertising</p>
+                            <p>Jobs</p>
+                            <p>Subscriptions</p>
+                            <p>Talent Workshops</p>
+                            <p>CPSC Certificates</p>
+                            <p>Ratings</p>
+                            <p>Shop Help</p>
+                            <p>Contact Us</p>
+                            <h5>SHOP</h5>
+                            <p>shop DC</p>
+                            <p>shop DC collectibles</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container flex">
+                    <div class="row">
+                        <div class="col">
+                            <h5>SITES</h5>
+                            <p>DC</p>
+                            <p>MAD Magazine</p>
+                            <p>DC kids</p>
+                            <p>DC Universe</p>
+                            <p>DC Power Visa</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+
+            </div>
+                
+            <div class="container-right">
+                <img src="../assets/img/dc-logo-bg.png" height="450">
+            </div>
+                
+                
+
+                
+        </div>
+        
+
+    </main>
+
+
+
+
+
 </template>
-<style lang="">
-    
+<style lang="scss">
+    .position-relative{
+        position: relative;
+    }
+
+    .position-absolute{
+        position: absolute;
+        top: 130px;
+        width: 100%;
+        background-color: black;
+        color: white;
+    }
+    .position-2{
+        position: absolute;
+        top: 189px;
+        width: 100%;
+        background-color: blue;
+    }
+
+    .position-3{
+        position: absolute;
+        top: 315px;
+        width: 100%;
+        display: flex;
+        
+    }
+
+    .background{
+        background-image: url(../assets/img/footer-bg.jpg);
+    }
+
+    .container-left{
+        width: 720px;
+    }
+
+
+
 </style>
